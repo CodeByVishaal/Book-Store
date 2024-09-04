5ce797b2cee70dd75b567d7d1fc247bbd3611bc6
Vishaalcodre <vishaa369@gmail.com>
2024-09-04 18:35:04 +0530

    display of Publisher data

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app(config_type):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
 
     configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
 
--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -9,3 +9,11 @@ def get_book():
     books = Book.query.all()
 
     return render_template('home.html', books=books)
+
+@main.route('/display/publisher/<int:publisher_id>')
+def display_publisher(publisher_id):
+    publisher = Publication.query.filter_by(id=publisher_id).first()
+
+    publisher_books = Book.query.filter_by(pub_id = publisher_id).all()
+
+    return render_template('publisher.html', publisher=publisher, publisher_books=publisher_books)
