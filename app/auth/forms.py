93ae28ee7306415f62fcc8a8b24620b85274f46b
Vishaalcodre <vishaa369@gmail.com>
2024-09-10 15:52:07 +0530

    Implementation of Data Vaildation in Registration Form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
 
 class RegistrationForm(FlaskForm):
 
-    name = StringField("What's your Name")
-    email = StringField("What's your Email")
+    name = StringField("Name", validators=[DataRequired(), Length(min=5, max=30, message='name must contain 5-30 characters')])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), 
+        Length(8,20, message='password must contain  8-20 characters'),
+        EqualTo('confirm')
+        ])
+    confirm = PasswordField("Confirm Password", validators=[DataRequired(), 
+        EqualTo('password', message='Passwords must match')])
     submit = SubmitField("Register")
+
 
