8ae0e6ba92aeaf2a1cf3895e62f9f0ef076c52b6
Vishaalcodre <vishaa369@gmail.com>
2024-09-10 23:31:54 +0530

    Checking if email already exists

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.auth.models import User
 
+def email_exists(form, field):
+    if User.query.filter_by(user_email = field.data).first():
+        raise ValidationError('Email already exists')
 
 class RegistrationForm(FlaskForm):
 
     name = StringField("Name", validators=[DataRequired(), Length(5, 30, message='name must contain 5-30 characters')])
-    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    email = StringField("E-mail", validators=[DataRequired(), Email(), email_exists])
     password = PasswordField("Password", validators=[DataRequired(), 
         Length(8,20, message='password must contain  8-20 characters'),
         EqualTo('confirm')
