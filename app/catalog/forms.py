7fc578e65100cd5269f3aa3ec327bec36323d676
Vishaalcodre <vishaa369@gmail.com>
2024-09-13 16:26:38 +0530

    Create Book Functionality

--- app/catalog/forms.py
+++ app/catalog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from app.auth.models import User
 
@@ -8,3 +8,14 @@ class EditForm(FlaskForm):
     format = StringField('Format', validators=[DataRequired()])
     num_pages = StringField('Pages', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class CreateForm(FlaskForm):
+    title = StringField('Title',  validators=[DataRequired()])
+    author = StringField('Author',  validators=[DataRequired()])
+    avg_rating = StringField('Rating', validators=[DataRequired()])
+    format = StringField('Format', validators=[DataRequired()])
+    img_url = StringField('Image',  validators=[DataRequired()])
+    num_pages = StringField('Pages', validators=[DataRequired()])
+    pub_id = IntegerField('Publisher ID', validators=[DataRequired()])
+    submit = SubmitField('Create Book')
+
--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -3,7 +3,7 @@ from app.catalog import main
 from app import db
 from app.catalog.models import Book, Publication
 from flask_login import login_required
-from .forms import EditForm
+from .forms import EditForm, CreateForm
 
 
 
@@ -34,7 +34,7 @@ def delete_book(book_id):
     
     return  render_template('delete_book.html', book=book, book_id=book_id)
 
-@main.route('/book/edit/<book_id>', methods=['GET', 'POST'])
+@main.route('/edit/book/<book_id>', methods=['GET', 'POST'])
 @login_required
 def edit_book(book_id):
     book = Book.query.get(book_id)
@@ -49,3 +49,37 @@ def edit_book(book_id):
         return redirect(url_for('main.display_books'))
     
     return render_template('edit_book.html', form=form)
+
+@main.route('/create/book/<pub_id>', methods=['GET', 'POST'])
+@login_required
+def  create_book(pub_id):
+    form = CreateForm()
+    form.pub_id.data = pub_id
+
+    if form.validate_on_submit():
+        book = Book(
+            title=form.title.data, 
+            author=form.author.data,
+            book_format=form.format.data,
+            num_pages=form.num_pages.data,
+            avg_rating=form.avg_rating.data,
+            image=form.img_url.data,
+            pub_id=form.pub_id.data
+            )
+        
+        # Check if the image already exists in the database
+        existing_book = Book.query.filter_by(image=form.img_url).first()
+        if existing_book:
+            flash('Book with this image already exists')
+            return redirect(url_for('main.create_book'))
+        
+        db.session.add(book)
+        db.session.commit()
+        flash("Book Created Successfully")
+        return(redirect(url_for('main.display_publisher',  publisher_id=pub_id)))
+
+    
+    return render_template('create_book.html', form=form,  pub_id=pub_id)
+
+
+
